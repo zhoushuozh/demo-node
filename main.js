f4cc51277cc993de984b66525d89f1f5ccf96cda
zhoushuo <zhoushuozh@gmail.com>
2018-04-30 22:19:12 +0800

    jsonp

---
+++ main.js
@@ -0,0 +1,20 @@
+button.addEventListener('click', (e)=>{
+	let script = document.createElement('script')
+	let functionName = 'query' + parseInt(Math.random()*100000,10)
+	window[functionName] = function(){
+		amount.innerText = parseInt(amount.innerText,10) - 1
+	}
+	script.setAttribute("type","text/javascript")
+	script.src = 'http://jsonpdemo2.com:8002/pay?callback=' + functionName
+	document.body.appendChild(script)
+	script.onload = (e) => {
+		e.currentTarget.remove()
+		delete window[functionName]
+	}
+	script.onerror = (e) => {
+		alert('fail')
+		e.currentTarget.remove()
+		delete window[functionName]
+	}
+})
+
